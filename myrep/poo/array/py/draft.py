a1511c2cecb67dd02c8dee5eba2ef2aeb83b99b5
Lucas <pedrolucas994234430@gmail.com>
2025-11-04 14:39:30 +0000

    array

---
+++ myrep/poo/array/py/draft.py
@@ -0,0 +1,12 @@
+# Criar um array vazio
+L1 = []
+
+# Criar um array preenchido
+L2: list[int] = [1, 2, 3]
+print(L2)
+
+#Obter o tamanho do array
+L3: list[int] = [1, 2, 3]
+print(len(L2))
+
+# Adicionar e remover elementos ao final
