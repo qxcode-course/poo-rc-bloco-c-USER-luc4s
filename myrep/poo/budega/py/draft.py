319b818f151eea0b86fa19abfa6c6b9896d8e1c8
Lucas <pedrolucas994234430@gmail.com>
2025-11-06 23:37:34 +0000

    2 class budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -9,10 +9,22 @@ class Cliente:
         return self._nome
 
 class Mercantil:
-    def __init__(self, num_caixas: int):
-        self.num_caixas = num_caixas
-        self.caixas = [None] * num_caixas
-        self.fila_espera = []
-    
+    def __init__(self, quantidade_caixas: int):
+        self.caixas: list[Cliente | None] = []
+        self.fila_espera: list[Cliente] = []
+        for _ in range(quantidade_caixas):
+            self.caixas.append(None)
+
     def __str__(self):
-        
+        caixa = ", ".join([str(cliente) if cliente else "-----" for cliente in self.caixas])
+        espera = ", ".join([str(cliente) for cliente in self.fila_espera])
+        return f"Caixas: [{caixa}]\n Espera: [{espera}]"
+    
+    def arrive(self, pessoa: Cliente):
+        self.fila_espera.append(pessoa)
+    
+    def call(self, index: int):
+        if self.caixas[index] is not None:
+            print("fail: caixa ocupado")
+        elif not self.fila_espera:
+            print("fail: sem clientes")
