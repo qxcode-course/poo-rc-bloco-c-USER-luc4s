4284a3f3736571c3d5eba5085ee49b7af33dcd29
Lucas <pedrolucas994234430@gmail.com>
2025-11-19 22:55:07 +0000

    pulapula

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,7 +1,7 @@
 class Kid:
     def __init__(self, age: int, name: str):
-        self.__age: int
-        self.__name: str
+        self.__age = age
+        self.__name = name
     
     def getAge(self) -> int:
         return self.__age
@@ -10,7 +10,7 @@ class Kid:
         return self.__name
 
     def setAge(self, age: int) ->  None:
-        self.__age = age
+        self.__age = age 
 
     def setName(self, name: str) -> None:
         self.__name = name
@@ -23,7 +23,23 @@ class Trampoline:
         self.playing = []
         self.waiting = []
 
-    def removeFromList(name: str, list_kids: list) -> kid | None:
-        for i, Kid in enumerate(list_kids):
-            if kid.name == name:
+    def arrive(self, name: str, age: int):
+        self.waiting.insert(0, kid(age, name))
                 
+    def enter(self):
+        if not self.waiting:
+            return
+        kid = self.waiting.pop(0)
+        self.playing.insert(0, kid)
+
+    def remove(self, name: str):
+        for i, kid in enumerate(self.waiting):
+            if kid.name == name:
+                self.waiting.pop(i)
+                return
+            
+        for i, kid in enumerate(self.playing):
+            if kid.name == name:
+                self.playing.pop(i)
+                return
+        print(f"fail: {name} nao esta no pula-pula") 
