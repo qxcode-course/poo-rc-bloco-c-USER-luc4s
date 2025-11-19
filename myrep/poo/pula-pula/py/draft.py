8af13faae8e3548aad5136b67e44808d3029c716
Lucas <pedrolucas994234430@gmail.com>
2025-11-19 14:47:31 +0000

    finalizando trampolim

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -9,11 +9,21 @@ class Kid:
     def getName(self) -> str:
         return self.__name
 
-    def setAge(self, age: int) -> int | None:
-        return self.__age
-    def setName(self, name: str) -> str | None:
-        return self.__name
+    def setAge(self, age: int) ->  None:
+        self.__age = age
+
+    def setName(self, name: str) -> None:
+        self.__name = name
     
     def __str__(self):
         return f"{self.__age}:{self.__name}"
     
+class Trampoline:
+    def __init__(self):
+        self.playing = []
+        self.waiting = []
+
+    def removeFromList(name: str, list_kids: list) -> kid | None:
+        for i, Kid in enumerate(list_kids):
+            if kid.name == name:
+                
