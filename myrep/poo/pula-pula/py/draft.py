fd1f37d9faeb6770b0e54c5962d49f6df225b572
Lucas <pedrolucas994234430@gmail.com>
2025-11-20 02:03:18 +0000

    main pulapula

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -16,7 +16,7 @@ class Kid:
         self.__name = name
     
     def __str__(self):
-        return f"{self.__age}:{self.__name}"
+        return f"{self.__name}:{self.__age}"
     
 class Trampoline:
     def __init__(self):
@@ -24,13 +24,19 @@ class Trampoline:
         self.waiting = []
 
     def arrive(self, name: str, age: int):
-        self.waiting.insert(0, Kid(age, name))
+        self.waiting.insert(0,Kid(age, name))
                 
     def enter(self):
         if not self.waiting:
             return
         kid = self.waiting.pop(0)
         self.playing.insert(0, kid)
+
+    def leave(self):
+        if not self.playing:
+            return
+        kid = self.playing.pop(0)
+        self.waiting.insert(0, kid)
 
     def remove(self, name: str):
         for i, kid in enumerate(self.waiting):
@@ -42,3 +48,43 @@ class Trampoline:
             if kid.getName() == name:
                 self.playing.pop(i)
                 return
+        print(f"fail: {name} nao esta no pula-pula")
+        
+    def show(self):
+        w = ", ".join(str(kid) for kid in self.waiting)
+        p = ", ".join(str(kid) for kid in self.playing)
+        print(f"[{w}] => [{p}]")
+
+def main():
+    tramp = Trampoline()
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        if not line:
+            continue
+        print(f"${line}")
+        args = line.split()
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "arrive":
+            name = args[1]
+            age = int(args[2])
+            tramp.arrive(name, age)
+        
+        elif args[0] == "enter":
+            tramp.enter()
+        elif args[0] == "leave":
+            tramp.leave()
+        elif args[0] == "remove":
+            name = args[1]
+            tramp.remove(name)
+        elif args[0] == "show":
+            tramp.show()
+        else:
+            print("fail: comando invalido")
+
+main()
